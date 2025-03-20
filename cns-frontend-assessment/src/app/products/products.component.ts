220de1fa1aaab61c3fb5c22ea7ef09f7e162aff5
Your Name <you@example.com>
2025-03-21 09:45:46 +1000

    🍱 refactor: add error handling for failed product network requests

--- cns-frontend-assessment/src/app/products/products.component.ts
+++ cns-frontend-assessment/src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from './services/products.service';
 import { Product } from './types/product-types';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -9,11 +10,24 @@ import { Product } from './types/product-types';
 })
 export class ProductsComponent implements OnInit {
   public products: Product[];
+  public error = {
+    hasError: false,
+    message: ''
+  };
   constructor(private service: ProductsService) {}
 
   ngOnInit(): void {
-    this.service.getProducts().subscribe((response) => {
-      console.log(response);
+    this.service.getProducts().pipe(
+      catchError((error) => {
+        // this would probably be better handled by sentry or some other error tracking service
+        console.error(error);
+        this.error = {
+          hasError: true,
+          message: 'An error occurred while fetching products'
+        };
+        return [];
+      })
+    ).subscribe((response) => {
       this.products = response.data.products;
     });
   }
