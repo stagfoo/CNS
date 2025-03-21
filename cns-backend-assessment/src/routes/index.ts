37ff26b20458cda3b4cb25a2436012d77c702848
Your Name <you@example.com>
2025-03-21 23:19:11 +1000

    ✨ feature: add the product network calls to the handler and add error handler on the bottom

--- cns-backend-assessment/src/routes/index.ts
+++ cns-backend-assessment/src/routes/index.ts
@@ -1,14 +1,27 @@
 import { Router, Request, Response } from 'express';
 import { getProducts, getProductDetails } from '../lib/products';
+import { ProductDetailsNetworkResponse } from '../types/productTypes';
+import { errorMiddleware } from '../lib/middleware';
+import { productDetailsNormalizer, productNormalizer } from '../lib/normalizers';
 
 const router: Router = Router();
 
 router.get('/products', async (req: Request, res: Response, next: (e: Error) => void): Promise<any> => {
-    
-});
+    try {
+        const products = await getProducts();
+        res.status(200).send(products);
+    } catch (e) {
+        next(e)
+    }
+}, errorMiddleware);
 
-router.get('/productDetails', async (req: Request, res: Response, next: (e: Error) => void): Promise<any> => {
-    
-});
+router.get('/productDetails/:id', async (req: Request, res: Response, next: (e: Error) => void): Promise<any> => {
+    try {
+        const product = await getProductDetails(req.params.id);
+        res.status(200).send(product);
+    } catch (e) {
+        next(e)
+    }
+}, errorMiddleware);
 
 export default router;
