5a45d099f374d5e12c2d8fada7a8eedc0bf24dc8
Your Name <you@example.com>
2025-03-21 23:18:37 +1000

    ✨ feature: add network function with normalizer outputs

--- cns-backend-assessment/src/lib/products.ts
+++ cns-backend-assessment/src/lib/products.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { Product, ProductDetails } from "../types/productTypes";
+import { NetworkProduct, NetworkProductDetails, Product, ProductDetails } from "../types/productTypes";
+import { productDetailsNormalizer, productNormalizer } from "./normalizers";
+
+
+
+
 
 /**
  * This function should contain code to retrieve the product data
@@ -8,7 +13,18 @@ import { Product, ProductDetails } from "../types/productTypes";
 export async function getProducts(): Promise<Product[]> {
     const url: string = 'https://api.commbank.com.au/public/cds-au/v1/banking/products';
     const headers = { 'x-v': 3 };
-    return Promise.resolve([]); // replace line - stand-in return
+    try {
+        const response = await axios.get(url, { headers });
+        const products = response.data?.data?.products.map(productNormalizer);
+        return products
+    } catch (error) {
+        const commbankError = error?.response?.data?.errors ?? null;
+        throw {
+            status: error.status,
+            error: commbankError,
+            message: 'Failed to fetch products'
+        };
+    }
 }
 
 /**
@@ -18,6 +34,17 @@ export async function getProducts(): Promise<Product[]> {
  */
 export async function getProductDetails(product_id: string): Promise<ProductDetails> {
     const url: string = 'https://api.commbank.com.au/public/cds-au/v1/banking/products';
-    const headers = { 'x-v': 3 };
-    return Promise.resolve({}); // replace line - stand-in return
+    const headers = { 'x-v': 4 };
+    try {
+        const response = await axios.get(`${url}/${product_id}`, { headers });
+        const product = productDetailsNormalizer(response.data?.data);
+        return product
+    } catch (error) {
+        const commbankError = error?.response?.data?.errors ?? null;
+        throw {
+            status: error.status,
+            error: commbankError,
+            message: `Failed to fetch single product [${product_id}]`
+        };
+    }
 }
